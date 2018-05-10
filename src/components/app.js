a6430ba246841ec285c7ca597af633a715ce678d
TJ Clark <mav@TJs-Mac-mini.local>
2018-05-10 12:52:32 -0700

    created madLib form component

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MadlibForm from './madlib_form';
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
               Fill out the fields below and click the generate button <br/> to see the Mad Lib story.
             </div>
           </div>
+          <MadlibForm />
         </div>
       </div>
     );
